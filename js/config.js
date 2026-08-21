3fedbabc96a3979bc8aac11def79dd2b3c5b69e1
Lifx Mode Safe <alif.diaz257@sma.belajar.id>
2026-08-21 16:14:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   GITHUB_CLIENT_ID: "Ov23liuWHwba08Mw5QjH",
   GITHUB_REDIRECT_URI: window.location.origin + window.location.pathname,
-  CLOUDFLARE_TURNSTILE_SITE_KEY: "0x4AAAAAAEWjPTyUhhf08lGQ",
+  CLOUDFLARE_TURNSTILE_SITE_KEY: "0x4AAAAAAEXd9koMpXIqq4ey",
   WORKER_BASE_URL: "https://codeflow-worker.alif-diaz257.workers.dev",
   STORAGE_PREFIX: "codeflow_",
 };
